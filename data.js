e88fc24b5bd75edf416caa414fb2b0db944a9a8b
Tavo Cruz <madecruz2@gmail.com>
2023-07-05 20:47:15 -0700

    backend

---
+++ data.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+        url: 'https://player.vimeo.com/external/314181352.sd.mp4?s=d2cd7a37f6250cd543e6d13209730b4bcf242130&profile_id=164&oauth2_token_id=57447761',
+        channel: "1tav0",
+        description: "Dummy data",
+        song: "Bori - 6ix9ine",
+        likes: "234",
+        messages: "123",
+        shares: "500"
+    },
+    {
+        url: 'https://player.vimeo.com/external/314181352.sd.mp4?s=d2cd7a37f6250cd543e6d13209730b4bcf242130&profile_id=164&oauth2_token_id=57447761',
+        channel: "1tav0",
+        description: "Dummy data",
+        song: "Bori - 6ix9ine",
+        likes: "234",
+        messages: "123",
+        shares: "500"
+    },
+    {
+        url: 'https://player.vimeo.com/external/314181352.sd.mp4?s=d2cd7a37f6250cd543e6d13209730b4bcf242130&profile_id=164&oauth2_token_id=57447761',
+        channel: "1tav0",
+        description: "Dummy data",
+        song: "Bori - 6ix9ine",
+        likes: "234",
+        messages: "123",
+        shares: "500"
+    },
+]
+
+export default data;
---
+++ server.js
@@ -0,0 +1,65 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors';
+import Data from './data.js'
+import Videos from './dbModel.js';
+
+//app config
+const app = express();
+dotenv.config()
+
+//middleware
+app.use(cors())
+app.use(express.json());
+// app.use((req, res, next) => {
+//     res.setHeaders("Allow-Control-Allow-Origin", "*"),
+//     res.setHeaders("Access-Control-Allow-Headers","*"),
+//     next();
+// })
+//DB config & listen
+const port = process.env.PORT || 9001;
+mongoose.connect(process.env.mongodb)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`);
+        }) 
+        console.log("connected to mongodb successfully");
+    })
+    .catch(error => {
+        console.log(error);
+    })
+        
+//api endpoints
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+})
+
+app.get('/v1/posts', (req, res) => {
+    res.status(200).send(Data)
+})
+
+app.post('/v2/posts', (req, res) => {
+    //POST request is to ADD DATA to the database
+    // It will let us ADD a video DOCUMENT to the videos COLLECTION
+    const dbVideos = req.body;
+
+    Videos.create(dbVideos)
+            .then(data => {
+                res.status(201).send(data);
+            })
+            .catch(error => {
+                res.status(500).send(error);
+            })
+})
+
+app.get('/v2/posts', (req, res) => {
+    Videos.find()
+            .then( data => {
+                res.status(200).send(data);
+            })
+            .catch(error => {
+                res.status(500).send(error);
+            })
+})
+//5:14
